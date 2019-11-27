d90f2b9766b3216378355f062cc277fd6496d8af
dcsordas <david.csordas@gmail.com>
2019-11-27 17:36:50 +0100

    flask: updated simple API server to runnable module, updated README

--- flask/README.md
+++ flask/README.md
@@ -1,2 +1,30 @@
 Flask code samples
 =
+
+REST API examples for Flask.
+
+
+Installation
+-
+
+`pip install -r requirements.txt`
+
+
+Usage
+-
+
+`python -m <module> --help`
+
+Running tests:
+
+`python -m <module>.test`
+
+or:
+
+`python -m <module> --test`
+
+
+simple
+-
+
+Using module level functions only for end points.
---
+++ flask/simple/__main__.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+
+from . import api
+from lib import util
+
+
+def main(args):
+    for record in util.load_data(args.data):
+        api.source.add(record)
+    api.app.run(host=args.host, port=args.port)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Flask REST API (with simple module level end points).")
+    parser.add_argument(
+        '--host',
+        default='localhost',
+        help='server host (default: %(default)s)')
+    parser.add_argument(
+        '--port',
+        default=api.PORT,
+        help='server port (default: %(default)s)')
+    parser.add_argument(
+        '--test',
+        action='store_true',
+        default=False,
+        help='run tests'
+    )
+
+    parser.add_argument(
+        '--data',
+        default=os.path.join(util.DATA_DIR, 'input.json'),
+        metavar='FILE',
+        help='path to JSON data file (default: %(default)s)')
+    args = parser.parse_args()
+
+    # test
+    if args.test:
+        from . import test
+        test.main()
+
+    # run server
+    else:
+        main(args)
